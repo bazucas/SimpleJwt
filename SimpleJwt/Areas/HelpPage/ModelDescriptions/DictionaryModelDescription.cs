850fb07a180c8294f8dc452c937cea16b2ffa502
Luis Inacio <luis.inacio@gmail.com>
2018-06-24 22:38:20 +0100

    Add project files.

---
+++ SimpleJwt/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SimpleJwt.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SimpleJwt/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SimpleJwt.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
